93440089ae828e59a3b53e192fe26567ac3a9c7d
Pauline Muigai <wanjirumuigai@gmail.com>
2023-03-16 12:54:08 +0300

    completed 3 out of 4 exercises

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".") ? true: false
     
   end
+  
 
   def question?
-
+    self.end_with?("?") ? true: false
   end
+ 
 
   def exclamation?
-
+    self.end_with?("!") ? true: false
   end
 
-  def count_sentences
-
+  def count_sentences(word)
+   a = word.split(/\b[\.|\?|\!]/)
+   
+   b = a.map do |w| 
+    w unless w.scan(/\w/).join("").length == 0
+    
+   end
+  p b
   end
+ 
 end
+word = String.new
+word.count_sentences("This, well, is a sentence. This is too!! And so is this, I think? Woo...")
+
